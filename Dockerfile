4004b9aab45523e4aad4ac2c74b48676a11db121
timf34 <farrelti@tcd.ie>
2023-06-11 02:23:16 +0100

    working background task!!!

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ COPY . /app
 RUN apt-get update
 RUN pip install --no-cache-dir -r requirements.txt
 EXPOSE 5000
-CMD ["gunicorn", "app:app", "-b", "0.0.0.0:5000", "-k", "gunicorn.workers.geventlet.EventletWorker"]
+# CMD ["gunicorn", "app:app", "-b", "0.0.0.0:5000", "-k", "gunicorn.workers.geventlet.EventletWorker"]
+CMD ["gunicorn", "-k", "eventlet", "-w", "1", "-b", "0.0.0.0:5000", "app:app"]
--- app.py
+++ app.py
@@ -1,21 +1,37 @@
 import logging
+import eventlet
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key'
-app.logger.setLevel(logging.INFO)
-# TODO: note there's no CORS here; implement this and test in a bit.
-socketio = SocketIO(app, logger=True, engineio_logger=True)
+socketio = SocketIO(logger=True, engineio_logger=True, async_mode='eventlet', cors_allowed_origins="*")
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'secret_key'
+    app.logger.setLevel(logging.INFO)
 
-@socketio.on('message')
-def handle_message(message):
-    app.logger.info('Received message: %s', message)
-    socketio.emit('message', message)
+    socketio.init_app(app)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @socketio.on('message')
+    def handle_message(message):
+        app.logger.info('Received message: %s', message)
+        socketio.emit('message', message)
+
+    def background_task_example():
+        """Example of how to use start_background_task."""
+        while True:
+            socketio.sleep(10)
+            app.logger.info("Background task!")
+            socketio.emit('message', 'This is a message from the background task.')
+
+    socketio.start_background_task(background_task_example)
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
     socketio.run(app)
